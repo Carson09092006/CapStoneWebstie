703d34e936a022b54e29fd8759b4342b03a9a7c9
Carson09092006 <cl1103122@student.francistuttle.edu>
2025-03-27 14:02:59 -0500

    Starting to add code to read the csv file

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13d75852183cec68f31db86e47479804001341ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e56901435cd611b042286a71224469491e45d74")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
