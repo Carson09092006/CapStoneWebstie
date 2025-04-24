1266988b3c8d6ce865cd1c9a98dc495e9b1c7181
Carson09092006 <cl1103122@student.francistuttle.edu>
2025-04-24 14:08:58 -0500

    working on the table

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fcceec9d6dc2406b264539a639bef06d45bacf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64159465ae80f06941b7b02e20361ea5e1f9d51")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
